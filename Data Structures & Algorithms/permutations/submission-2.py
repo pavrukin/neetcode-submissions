f6f1ae6e93aa74ef4bc0400048d71a347af5d753
Pavel R <129773262+pavrukin@users.noreply.github.com>
2026-07-18 19:15:53 +0200

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        seen_permutations=set()
+        result=[]
+
+        def check_if_seen(permutation):
+            key=tuple(permutation)
+            if key in seen_permutations: 
+                return True
+            seen_permutations.add(key) 
+            return False
+        
+        def backtracking(current_combo, remain_list):
+            # if nothing left we check the compbination
+            if not remain_list:
+                if not check_if_seen(current_combo):
+                    result.append(list(current_combo))
+                return
+            
+            for i in range(len(remain_list)):
+                value_to_check=remain_list[i]
+                current_combo.append(value_to_check)
+                remain_choices=remain_list[:i]+remain_list[i+1:]
+                backtracking(current_combo,remain_choices)
+                # be sure to assign list when pass to result
+                # otherwise this pop will make result [] 
+                # and all return will be filled with [] [] [] ...
+                current_combo.pop()
+            
+        backtracking([],nums)
+
+        return result
+
+
+        
+        
+        
+
+
+
