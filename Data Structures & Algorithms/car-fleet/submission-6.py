34fd0a501bb5d0772dff3faaeeb004df104c6b94
Pavel R <129773262+pavrukin@users.noreply.github.com>
2026-07-22 23:27:53 +0200

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_speed=(sorted(list(zip(position,speed)), reverse=True))
+        sorted_pos, sorted_speed = map(list, zip(*pos_speed))
+        time_to_target=[]
+        for k in range(len(sorted_pos)): 
+            time_to_target.append((target-sorted_pos[k])/sorted_speed[k])
+        fleets=len(time_to_target)
+
+        for i in range(len(time_to_target)-1):
+            if time_to_target[i]>=time_to_target[i+1]:
+                fleets-=1 
+                time_to_target[i+1]=time_to_target[i]
+
+        return fleets
